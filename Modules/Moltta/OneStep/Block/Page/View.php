2f7c782912c762f128bde051132e56ff7f42ae46
Niam Moltta <lastralab@gmail.com>
2018-08-19 13:30:50 -0600

    Add files via upload

---
+++ Modules/Moltta/OneStep/Block/Page/View.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Moltta\OneStep\Block\Page;
+
+class View extends \Magento\Framework\View\Element\Template {
+
+    public function __construct(\Magento\Catalog\Block\Product\Context $context, array $data = []) {
+
+        parent::__construct($context, $data);
+
+    }
+
+
+    protected function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+
+}
